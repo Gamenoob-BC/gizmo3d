ed896f5ba59d540ceae66c6f7a68a8a7551cc201
Gamenoob-BC <143728735+Gamenoob-BC@users.noreply.github.com>
2023-11-04 15:36:02 +0100

    Updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,23 @@
-// Import required crates and libraries
-extern crate wgpu;
-extern crate winit;
-extern crate physx;
-extern crate cpal;
+// src/lib.rs
+#![feature(custom_inner_attributes)]
+
 use pyo3::prelude::*;
+use wgpu::Instance as WgpuInstance;
+use physx::*;
+use cpal::traits::{DeviceTrait, HostTrait};
 
-// Define your game engine struct
-pub struct Gizmo {
-    // Define fields for wgpu, PhysX, CPAL, and other engine components
-    wgpu_instance: wgpu::Instance,
-    wgpu_device: wgpu::Device,
-    wgpu_queue: wgpu::Queue,
-    physx_world: physx::PxWorld,
-    cpal_host: cpal::Host,
-    // Add more fields for your engine's systems and data structures
-}
-
-impl Gizmo {
-    pub fn new() -> Self {
-        // Initialize wgpu instance, device, and queue
-        let wgpu_instance = wgpu::Instance::new(wgpu::Backends::all());
-
-        let wgpu_device = wgpu_instance.create_device(
-            &wgpu::DeviceDescriptor {
-                // Configure your device descriptor
-                // ...
-            }
-        );
-
-        let wgpu_queue = wgpu_device.get_queue();
-
-        // Initialize PhysX physics world
-        let physx_world = physx::PxWorld::new(
-            // Configure PhysX parameters, gravity, and simulation settings
-            // ...
-        );
-
-        // Initialize CPAL audio host
-        let cpal_host = cpal::default_host();
-
-        // Additional setup for your engine's components
-        // ...
-
-        Gizmo {
-            wgpu_instance,
-            wgpu_device,
-            wgpu_queue,
-            physx_world,
-            cpal_host,
-            // Initialize other engine fields here
-            // ...
-        }
-    }
-
-    pub fn run(&mut self) {
-        // Main game loop
-        // Handle input, physics simulation, rendering, audio, etc.
-        // ...
-
-        // Update your engine components within the loop
-        // ...
-    }
-
-    // Add more methods to handle different aspects of your engine
-    // ...
-}
-
-// Define Python API using PyO3
 #[pymodule]
 fn gizmo_engine(_py: Python, m: &PyModule) -> PyResult<()> {
-    // Define Python bindings for your engine's methods and functionalities
-    m.add_class::<Gizmo>()?;
+    let _ = m.add_function(wrap_pyfunction!(create_game_object, m)?);
+    let _ = m.add_function(wrap_pyfunction!(move_game_object, m)?);
     Ok(())
 }
+#[pyfunction]
+fn create_game_object(name: &str)  {
 
-// Implement Python methods for your engine
-#[pymethods]
-impl Gizmo {
-    #[new]
-    fn new() -> Self {
-        Gizmo::new()
-    }
+}
 
-    fn run(&mut self) {
-        self.run();
-    }
+#[pyfunction]
+fn move_game_object(name: &str, x: f32, y: f32, z: f32) {
 
-    // Add more Python methods that correspond to your engine's functionalities
-    // ...
 }
